3c13f2e7c6d05e0226e50c30f6813941a49865e6
Zhehao Wang <wangzhehao410305@gmail.com>
2018-11-18 19:17:48 -0500

    src: add insta text feed automation

--- src/helpers.py
+++ src/helpers.py
@@ -1,6 +1,8 @@
 import os
-from selenium.common.exceptions import InvalidSessionIdException
+import re
+from datetime import datetime, timedelta
 
+from selenium.common.exceptions import InvalidSessionIdException
 
 ANDROID_APP_PATH = 'http://appium.github.io/appium/assets/ApiDemos-debug.apk' if os.getenv(
     'SAUCE_LABS') else os.path.abspath('../bin/instagram-70-0-0-22-98.apk')
@@ -42,3 +44,103 @@ def __save_log_type(driver, device_logger, calling_request, type):
         for data in logcat_data:
             data_string = '{}:  {}'.format(data['timestamp'], data['message'])
             logcat_file.write((data_string + '\n').encode('UTF-8'))
+
+def get_structured_feeds(text_feeds, offset_time = datetime.now()):
+    structured_feeds = []
+    CONJUNCTION = 'and'
+
+    for feed in text_feeds:
+        parts = feed.split()
+
+        time = parts[-1].strip()
+        
+        subject = [parts[0].strip()]
+        rest_idx = 1
+
+        # a and b liked c's post. 2h
+        # TODO: this does not account for 'a, b, and 3 others liked c's post
+        if len(parts) > 1 and parts[1] == CONJUNCTION:
+            rest_idx = 3
+            subject.append(parts[2])
+
+        text = ' '.join(parts[rest_idx:-1])
+        text.strip()
+
+        post_time = offset_time
+        if time.endswith('d'):
+            post_time = offset_time - timedelta(days = int(time[:-1]))
+        elif time.endswith('h'):
+            post_time = offset_time - timedelta(hours = int(time[:-1]))
+        elif time.endswith('m'):
+            post_time = offset_time - timedelta(minutes = int(time[:-1]))
+        elif time.endswith('s'):
+            post_time = offset_time - timedelta(seconds = int(time[:-1]))
+        else:
+            print('unrecognized time format: ' + time)
+
+        actions = ['liked', 'started following', 'shared']
+
+        def parse_following(text):
+            targets = text.split(',')
+            and_targets = targets[-1].split()
+            res = [target.strip() for target in targets[:-1]]
+            if len(and_targets) >= 3 and and_targets[1].strip() == CONJUNCTION:
+                res.append(and_targets[0])
+                
+                if len(and_targets) > 3:
+                    count = int(and_targets[2])
+                    res += ['unknown' for i in range(count)]
+                else:
+                    res.append(and_targets[2].rstrip('.'))
+            return res
+
+        patterns = [{
+                # liked c's post.
+                'string': actions[0] + " ([^' ]*?)'s posts?.",
+                'action': lambda x : {
+                    'action': actions[0].replace(' ', '_'),
+                    'target': [x.group(1)]
+                }
+            }, {
+                # liked x posts.
+                'string': actions[0] + ' ([0-9]+) posts?.',
+                'action': lambda x : {
+                    'action': actions[0].replace(' ', '_'),
+                    'count': int(x.group(1))
+                }
+            }, {
+                # started following a, b, c.
+                # started following a, b, and x others.
+                'string': actions[1] + ' (.*)',
+                'action': lambda x : {
+                    'action': actions[1].replace(' ', '_'),
+                    'target': parse_following(x.group(1))
+                }
+            }, {
+                # shared x posts at l.
+                'string': actions[2] + ' ([0-9]+) posts at ([^ ]*?)',
+                'action': lambda x : {
+                    'action': actions[2].replace(' ', '_'),
+                    'count': int(x.group(1)),
+                    'target': [x.group(2)]
+                }
+            }]
+        
+        match_found = False
+        structured_feed = {}
+
+        for pattern in patterns:
+            res = re.match(pattern['string'], text)
+            if res:
+                match_found = True
+                structured_feed = pattern['action'](res)
+                break
+
+        if not match_found:
+            print('failed to match: ' + text)
+        structured_feed['time'] = post_time
+        structured_feed['subject'] = subject
+
+        structured_feeds.append(structured_feed)
+
+    return structured_feeds
--- src/insta.py
+++ src/insta.py
@@ -3,27 +3,38 @@ import pytest
 import os
 import textwrap
 import json
+from datetime import datetime
 
 from appium import webdriver
-from helpers import take_screenhot_and_logcat, ANDROID_APP_PATH, EXECUTOR
+from appium.webdriver.common.touch_action import TouchAction
 
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+from helpers import take_screenhot_and_logcat, EXECUTOR, get_structured_feeds
 
 class TestAndroidBasicInteractions():
     PACKAGE = 'com.instagram.android'
     MAIN_ACTIVITY = '.activity.MainTabActivity'
+    WAIT_ACTIVITY = 'com.instagram.nux.activity.SignedOutFragmentActivity'
+
+    HEART_XPATH = '//android.widget.FrameLayout[@content-desc="Activity"]'
+    FOLLOWING_XPATH = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.TextView[1]'
 
     @pytest.fixture(scope='function')
     def driver(self, request, device_logger):
         calling_request = request._pyfuncitem.name
         driver = webdriver.Remote(
-            command_executor=EXECUTOR,
-            desired_capabilities={
-                'app': ANDROID_APP_PATH,
-                'platformName': 'Android',
-                'automationName': 'UIAutomator2',
-                'platformVersion': os.getenv('ANDROID_PLATFORM_VERSION') or '9',
-                'deviceName': os.getenv('ANDROID_DEVICE_VERSION') or 'Android',
-                'appActivity': self.MAIN_ACTIVITY
+            command_executor = EXECUTOR,
+            desired_capabilities = {
+              "platformName": "Android",
+              "platformVersion": "9",
+              "deviceName": "Android",
+              "automationName": "UIAutomator2",
+              "app": "/Users/zwang541/personal/themistalkles/bin/instagram-70-0-0-22-98.apk",
+              "appWaitPackage": self.PACKAGE,
+              "appWaitActivity": self.WAIT_ACTIVITY
             }
         )
 
@@ -36,27 +47,81 @@ class TestAndroidBasicInteractions():
         driver.implicitly_wait(10)
         return driver
 
-    def test_open_instagram(self, driver):
+    def test_open_instagram_and_print_feeds(self, driver):
         with open('../credentials/insta.json') as cred:
             content = json.loads(cred.read())
             acc_usrname = content['username']
             acc_pwd = content['password']
 
-        driver.start_activity(self.PACKAGE, self.MAIN_ACTIVITY)
+        driver.start_activity(
+            self.PACKAGE,
+            self.MAIN_ACTIVITY,
+            app_wait_activity = self.WAIT_ACTIVITY)
 
-        driver.implicitly_wait(5)
-        login_button = driver.find_element_by_id('log_in')
+        # log in
+        wait = WebDriverWait(driver, 20)
+        curr_wait = wait.until(EC.element_to_be_clickable((By.ID, 'log_in_button')))
+
+        login_button = driver.find_element_by_id('log_in_button')
         login_button.click()
 
-        driver.implicitly_wait(5)
+        curr_wait = wait.until(EC.element_to_be_clickable((By.ID, 'login_username')))
         username = driver.find_element_by_id('login_username')
         username.send_keys(acc_usrname)
 
-        password = driver.find_element_by_id('login_password')
+        password = driver.find_element_by_id('password')
         password.send_keys(acc_pwd)
 
-        driver.implicitly_wait(5)
         next_button = driver.find_element_by_id('next_button')
         next_button.click()
+
+        # go to 'following'
+        curr_wait = wait.until(EC.element_to_be_clickable((By.XPATH, self.HEART_XPATH)))
+        heart_icon = driver.find_element(By.XPATH, self.HEART_XPATH)
+        heart_icon.click()
+
+        curr_wait = wait.until(EC.element_to_be_clickable((By.ID, 'fixed_tabbar_tabs_container')))
+        following_button = driver.find_element(By.XPATH, self.FOLLOWING_XPATH)
+        following_button.click()
+
+        driver.implicitly_wait(2)
+        text_feeds = []
+
+        def record_feeds(driver, text_feeds):
+            text_elements = driver.find_elements(By.ID, 'row_newsfeed_text')
+            for elem in text_elements:
+                if not elem.text in text_feeds:
+                    text_feeds.append(elem.text)
+            # we only care about collecting the feed till 1d ago, assuming daily
+            # run
+            if len(text_feeds) > 0:
+                last = text_feeds[-1].split()[-1]
+                if not last.endswith('h'):
+                    return False
+            return True
+
+        # record feeds in 'following'
+        scroll_further = True
+        while scroll_further:
+            driver.swipe(470, 1400, 470, 950, 330)
+            driver.implicitly_wait(1)
+            scroll_further = record_feeds(driver, text_feeds)
+
+        print(text_feeds)
+        with open('text_feeds_' + str(datetime.now()) + '.log', 'w') as writefile:
+            writefile.write(text_feeds)
+
+        # process structured feeds
+        structured_feeds = get_structured_feeds(text_feeds)
+        with open('structured_feeds_' + str(datetime.now()) + '.log', 'w') as writefile:
+            writefile.write(structured_feeds)
+
+        # To get thumbnails from the ImageView elements, it seems the best way
+        # for now is to crop screenshots
+
+        # https://stackoverflow.com/questions/13832322/how-to-capture-the-screenshot-of-a-specific-element-rather-than-entire-page-usin
+        # media_elements = driver.find_elements(By.XPATH, '//android.widget.ImageView')
+        # for elem in media_elements:
+        #     print(elem)
 
         
